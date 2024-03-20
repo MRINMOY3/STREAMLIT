b5ecdba124ec8a6085e149c262c79ec7f4df97b8
Mrinmoy Bordoloi <86611284+MRINMOY3@users.noreply.github.com>
2024-03-20 23:59:53 +0530

    Update MAIN_APP.py

--- MAIN_APP.py
+++ MAIN_APP.py
@@ -1,6 +1,11 @@
 import sklearn.ensemble as ensemble
-from sklearn.model_selection import train_test_split
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler, FunctionTransformer
+from sklearn.svm import SVC
 import streamlit as st
 import pandas as pd
 import plotly.express as px
