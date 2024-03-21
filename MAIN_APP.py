f0aa029c92b29add8bc081e3942c2391ebac94a4
Mrinmoy Bordoloi <86611284+MRINMOY3@users.noreply.github.com>
2024-03-21 17:17:06 +0530

    Update MAIN_APP.py

--- MAIN_APP.py
+++ MAIN_APP.py
@@ -1,4 +1,4 @@
-import sklearn.ensemble as ensemble
+import sklearn.ensemble as ensemble  
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
